aef576ced62c6668c82834ec512f0c9a402951c5
Ismael Barros² <ismael@imasdetres.com>
2017-08-07 17:10:29 +0200

    Testing MutationObserver

--- assets/javascripts/redmine_select2_all_the_things.js
+++ assets/javascripts/redmine_select2_all_the_things.js
@@ -1,3 +1,16 @@
 $(function(){
-	$('select').select2({ width: 'off', dropdownAutoWidth : true });
+	// create an observer instance
+	var observer = new MutationObserver(function(mutations) {
+		//loop through the detected mutations(added controls)
+		mutations.forEach(function(mutation) {
+			//addedNodes contains all detected new controls
+			if (mutation && mutation.addedNodes) {
+				mutation.addedNodes.forEach(function(elm) {
+					//only apply select2 to select elements
+					$(elm).select('select').select2({ width: 'off', dropdownAutoWidth : true });
+				});
+			}
+		});
+	});
+        observer.observe($('body')[0], { childList: true, subtree: true });
 });
